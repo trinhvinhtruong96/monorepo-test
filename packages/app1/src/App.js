29072dc902a16eece87304b66c680bc4b3d45b5e
trinhvinhtruong96 <48130078+trinhvinhtruong96@users.noreply.github.com>
2022-11-15 01:12:40 +0700

    init mono repo

---
+++ packages/app1/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>app 1</h1>
+  );
+}
+
+export default App;
---
+++ packages/app2/src/App.js
@@ -0,0 +1,12 @@
+import App1 from 'app1/src/App'
+
+function App() {
+  return (
+    <>
+      <h1>app 2</h1>
+      <App1 />
+    </>
+  );
+}
+
+export default App;
